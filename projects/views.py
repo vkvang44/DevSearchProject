fef33ce7d526c320d9471243d42d129191e47367
Victor Vang <vkvang44@gmail.com>
2021-08-31 22:19:06 -0700

    created startapp projects

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def projects(request):
+    return render(request, 'projects/projects.html', {})
