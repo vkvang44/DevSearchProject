eeb1745a7137e28ebd111bb977144783d901a442
Victor Vang <vkvang44@gmail.com>
2021-09-01 19:43:31 -0700

    created models in the projects app
    Basic models to demonstrate knowledge of creating tables in database.

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from.models import Project
 
-# Register your models here.
+
+# Register your models here after you have created the tables in your models.py to see them in /admin
+from.models import Project
+
+admin.site.register(Project)
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, blank=True, null=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    # add in str method to get the title name to render in admin
+    def __str__(self):
+        return self.title
