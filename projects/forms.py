db16a024f728721b16cedffd296d2f3bfe0a1197
Victor Vang <vkvang44@gmail.com>
2021-09-02 22:36:02 -0700

    implemented template theme for add project page

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 
 class ProjectForm(ModelForm):
@@ -7,3 +8,13 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'description', 'demo_link', 'source_link', 'tags', 'featured_image']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        #self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder':'Project Name',})
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
