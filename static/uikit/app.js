9b1fb21d5f153463a22edeeacfd861f87469bf07
Victor Vang <vkvang44@gmail.com>
2021-09-06 12:58:39 -0700

    fixed search and pagination bug that would break the pagination

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
 });
 
-let alertWrapper = document.querySelectorAll('.alert');
-let alertClose = document.querySelectorAll('.alert__close');
 
-if(alertWrapper){
-    for (let i = 0; i < alertClose.length; i++) {
-     alertClose[i].addEventListener('click', ()=>{
-       alertWrapper[i].style.display = 'none'});
-       }
-  }
+let alertWrapper = document.querySelector('.alert')
+let alertClose = document.querySelector('.alert__close')
+
+if (alertWrapper) {
+  alertClose.addEventListener('click', () =>
+    alertWrapper.style.display = 'none'
+  )
+}
